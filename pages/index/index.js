889aacd76b18a9aa4aeded5e81d0527dfc00eb16
fengfengying <2119179562@qq.com>
2021-07-21 16:10:23 +0800

    首页界面完善

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,25 @@ const app = getApp()
 Page({
   data: {
     bannerList: [],
-    searchKey: ''
+    searchKey: '',
+    homeNarList: [
+      {
+        title: '国家馆',
+        img: '../../image/homeNav/nav2.png'
+      },
+      {
+        title: '品牌街',
+        img: '../../image/homeNav/nav3.png'
+      },
+      {
+        title: '新品',
+        img: '../../image/homeNav/new.png'
+      },
+      {
+        title: '热卖',
+        img: '../../image/homeNav/hot.png'
+      },
+    ]
   },
   // 事件处理函数
   bindViewTap() {},
